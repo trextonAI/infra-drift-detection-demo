930ec72a6e16a76697a1e89f494a2d6a7c8af330
trextonAI <shubham0007ai@gmail.com>
2026-05-06 14:29:46 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "drift-demo-rg"
+  location = "Central India"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "drift-demo-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "drift-demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
